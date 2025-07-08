141dfa570e3041322895934ffad04461207455bb
ip61-ua <ip61@alu.ua.es>
2025-07-08 22:43:14 +0200

    feat: Structurate tracker

---
+++ src/tracker/tracker.py
@@ -0,0 +1,39 @@
+from abc import ABC, abstractmethod
+from src.model.structures import Register
+
+
+class Tracker(ABC):
+    def __init__(self, link: str):
+        self.link = link
+
+    @staticmethod
+    def can_parse (link: str) -> bool:
+        raise NotImplemented()
+
+    @abstractmethod
+    def request (self) -> Register:
+        return NotImplemented()
+
+    @abstractmethod
+    def request_all (self) -> list[Register]:
+        return NotImplemented()
+
+    @abstractmethod
+    def request_raw (self) -> str:
+        return NotImplemented()
+
+    def get_engine (self) -> str:
+        return self.__class__.__name__
+
+
+class TrackerFactory:
+    def __cache_save (link: str):
+        pass
+
+    def __cache_load (link: str) -> (Tracker|None):
+        pass
+
+    def get_engine_by_link (link: str) -> (Tracker|None):
+        pass
+
+
